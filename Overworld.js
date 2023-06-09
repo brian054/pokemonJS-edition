74c805b74fb770ea66ffb0f3365fe12c4177bfd1
brian054 <brian.rowell54@gmail.com>
2023-06-09 10:20:20 -0400

    Added Sprites and GameObjects

--- Overworld.js
+++ Overworld.js
@@ -8,48 +8,29 @@ class Overworld {
     init() {
         console.log("Hello from the Overworld.", this);
 
-        // Load image into browser memory (under the hood)
+        // Load image into browser memory 
         const image = new Image();
         image.onload = () => { 
             this.ctx.drawImage(image, 0, 0) 
         };
         image.src = "/images/maps/DemoLower.png";
 
+        // Place game objects
+        const hero = new GameObject({
+            x: 5,
+            y: 7
+        })
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: "/images/characters/people/npc1.png"
+        })
 
-        const x = 5;
-        const y = 6;
-        // Shadow under Sprite 'hero' feet
-        const shadow = new Image();
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow, 
-                0, // left cut 
-                0, // top cut
-                32, // 32 x 32 cuts (width)
-                32, // height
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-            )
-        }
-        shadow.src = "/images/characters/shadow.png";
-
-        const hero = new Image();
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero, 
-                0, // left cut 
-                0, // top cut
-                32, // 32 x 32 cuts (width)
-                32, // height
-                x * 16 - 8,
-                y * 16 - 18,
-                32,
-                32
-            )
-        }
-        hero.src = "/images/characters/people/hero.png"
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc1.sprite.draw(this.ctx);
+        }, 200)
+        
 
     }
 }
---
+++ Sprite.js
@@ -0,0 +1,51 @@
+class Sprite {
+    constructor(config) {
+
+        // Set up image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;
+        }
+
+        // Shadow
+        this.shadow = new Image();
+        this.useShadow = true; // config.useShadow || false
+        if (this.useShadow) {
+            this.shadow.src = "/images/characters/shadow.png";
+        }
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+        
+
+        // Configure Animation and Initial State
+        this.animations = config.animations || {
+            idleDown: [ // 2d array of frames
+                [0,0]
+            ],
+            walkDown: [
+                [0,0], [1,0], [2,0], [3,0]
+            ]
+        }
+        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimationFrame = 0;
+
+        // Ref the game object
+        this.gameObject = config.gameObject;
+    }
+
+    draw(ctx) {
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.x * 16 - 18;
+
+        this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
+
+        // Make sure image loaded into memory before draw
+        this.isLoaded && ctx.drawImage(this.image,
+            0, 0,
+            32, 32, 
+            x, y,
+            32, 32)
+    }
+}
