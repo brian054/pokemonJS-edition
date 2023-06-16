6e902910c7030566c1284bfe8676fc3921bdeb7c
brian054 <brian.rowell54@gmail.com>
2023-06-15 21:26:31 -0400

    player sprite animation

--- Sprite.js
+++ Sprite.js
@@ -21,18 +21,52 @@ class Sprite {
 
         // Configure Animation and Initial State
         this.animations = config.animations || {
-            idleDown: [ // 2d array of frames
-                [0,0]
-            ],
-            walkDown: [
-                [0,0], [1,0], [2,0], [3,0]
-            ]
+            "idle-down" : [ [0,0] ], // 2d array of frames
+            "idle-right": [ [0,1] ],
+            "idle-up"   : [ [0,2] ],
+            "idle-left" : [ [0,3] ],
+            "walk-up"   : [ [1,2], [0,2], [3,2], [0,2] ],
+            "walk-down" : [ [1,0], [0,0], [3,0], [0,0] ],
+            "walk-left" : [ [1,3], [0,3], [3,3], [0,3] ],
+            "walk-right": [ [1,1], [0,1], [3,1], [0,1] ]
         }
-        this.currentAnimation = config.currentAnimation || "idleDown";
+        this.currentAnimation = "idle-right" // config.currentAnimation || "idle-down";
         this.currentAnimationFrame = 0;
+
+        // Time
+        this.animationFrameLimit = config.animationFrameLimit || 8; // # of game loop frames we want to show of the one cut in the sprite sheet
+        this.animationFrameProgress = this.animationFrameLimit;
 
         // Ref the game object
         this.gameObject = config.gameObject;
+    }
+
+    get frame() {
+        return this.animations[this.currentAnimation][this.currentAnimationFrame];
+    }
+
+    setAnimation(key) {
+        if (this.currentAnimation !== key) {
+            this.currentAnimation = key;
+            this.currentAnimationFrame = 0;
+            this.animationFrameProgress = this.animationFrameLimit;
+        }
+    }
+
+    updateAnimationProgress() {
+        // downtick frame progress counter
+        if (this.animationFrameProgress > 0) {
+            this.animationFrameProgress -= 1;
+            return;
+        }
+
+        // Reset the counter
+        this.animationFrameProgress = this.animationFrameLimit;
+        this.currentAnimationFrame += 1;
+
+        if (this.frame == undefined) {
+            this.currentAnimationFrame = 0;
+        }
     }
 
     draw(ctx) {
@@ -41,11 +75,16 @@ class Sprite {
 
         this.isShadowLoaded && ctx.drawImage(this.shadow, x, y)
 
+        const [frameX, frameY] = this.frame;
+
         // Make sure image loaded into memory before draw
         this.isLoaded && ctx.drawImage(this.image,
-            0, 0,
+            frameX * 32, frameY * 32,
             32, 32, 
             x, y,
-            32, 32)
+            32, 32
+        )
+
+        this.updateAnimationProgress();
     }
 }
